1d7b33dc7c53a6356b7c5c41a9bcffb931d1be22
Ryan Rozich <github@rozich.com>
2026-06-11 17:31:01 -0500

    feat(dev): CTL-1049 (E) rebuild bundle on top of rebased main (CTL-1024+CTL-1009)

---
+++ plugins/dev/scripts/orch-monitor/public/assets/use-scoped-board-snapshot-Dr_9Q9Er.js
@@ -0,0 +1,1 @@
+import{aM as t,aN as p,r,aO as c}from"./main-BWsxmdUW.js";function d(){const{payload:o,status:e}=t(),a=(o==null?void 0:o.repos)??[],{scope:s}=p(a);return{payload:r.useMemo(()=>o?c(o,s):null,[o,s]),status:e}}export{d as u};
